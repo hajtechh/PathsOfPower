5fbc9a4487c8f296c58aa8de5dd505caae5e22ac
Hanna Friborg <115992232+friborg@users.noreply.github.com>
2023-11-22 13:13:39 +0100

    Added model for Quest and Option

---
+++ PathsOfPower/Models/Quest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathsOfPower.Models
+{
+    public class Quest
+    {
+        public string Index { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<Option> Options { get; set; }
+    }
+}
