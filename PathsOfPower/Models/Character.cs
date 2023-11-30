3af22d316e15ccdee1d9c8eb4dca4aa5521a270d
Anna Nilsson-Niia <annanilssonniia@gmail.com>
2023-11-30 11:30:52 +0100

    Add Ienumerable of IInventoryItem in class Character

--- PathsOfPower/Models/Character.cs
+++ PathsOfPower/Models/Character.cs
@@ -1,7 +1,10 @@
-﻿namespace PathsOfPower.Models;
+﻿using PathsOfPower.Interfaces;
+
+namespace PathsOfPower.Models;
 
 public class Character
 {
     public string Name { get; set; }
     public int MoralitySpectrum { get; set; }
+    public IEnumerable<IInventoryItem> InventoryItems { get; set; }
 }
