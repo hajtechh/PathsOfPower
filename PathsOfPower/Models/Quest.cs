7a92936ae4711ce1b5d37b26c0d1d9e9297ce97e
Anna Nilsson-Niia <annanilssonniia@gmail.com>
2023-11-30 11:32:36 +0100

    Add property IInventoryItem in Quest

--- PathsOfPower/Models/Quest.cs
+++ PathsOfPower/Models/Quest.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PathsOfPower.Interfaces;
 
 namespace PathsOfPower.Models;
-
 public class Quest
 {
     public string Index { get; set; }
     public string Description { get; set; }
     public IEnumerable<Option>? Options { get; set; }
+    public IInventoryItem Item { get; set; }
 }
