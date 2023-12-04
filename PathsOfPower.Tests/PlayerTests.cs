36726714f07b07e44f6ad99caabcfffe22bdf32a
JonnaJK <96888686+JonnaJK@users.noreply.github.com>
2023-12-04 16:19:42 +0100

    Add a new test class for Player

---
+++ PathsOfPower.Tests/PlayerTests.cs
@@ -0,0 +1,28 @@
+﻿namespace PathsOfPower.Tests;
+
+public class PlayerTests
+{
+    [Fact]
+    public void AddInventoryItem()
+    {
+
+    }
+
+    [Fact]
+    public void ApplyMoralityScore()
+    {
+
+    }
+
+    [Fact]
+    public void ApplyPowerUpScore()
+    {
+
+    }
+
+    [Fact]
+    public void PerformAttack()
+    {
+
+    }
+}
