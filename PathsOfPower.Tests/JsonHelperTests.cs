9c2eaa6e87e3897a450c348d4fbf4fee8e804542
JonnaJK <96888686+JonnaJK@users.noreply.github.com>
2023-12-06 10:23:24 +0100

    Add test for functions in JsonHelper class

---
+++ PathsOfPower.Tests/JsonHelperTests.cs
@@ -0,0 +1,38 @@
+﻿namespace PathsOfPower.Tests;
+
+public class JsonHelperTests
+{
+    [Fact]
+    public void DeSerializeObjectShouldReturnNotNull()
+    {
+        // Arrange
+        var sut = new JsonHelper();
+        var quest = @"{""Index"":""1"",""Description"":""You are in a classroom at Hogwarts. What do you want to teach your students today?""}";
+
+        // Act
+        var actual = sut.Deserialize<Quest>(quest);
+
+        // Assert
+        Assert.NotNull(actual);
+        Assert.NotNull(actual.Description);
+    }
+
+    [Fact]
+    public void SerializeObjectShouldReturnNotNullOrEmpty()
+    {
+        // Arrange
+        var sut = new JsonHelper();
+        var quest = new Quest()
+        {
+            Index = "1",
+            Description = "You are in a classroom at Hogwarts. What do you want to teach your students today?"
+        };
+
+        // Act
+        var actual = sut.Serialize(quest);
+
+        // Assert
+        Assert.NotNull(actual);
+        Assert.NotEmpty(actual);
+    }
+}
