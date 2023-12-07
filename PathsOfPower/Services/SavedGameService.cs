9589cc8150cc6a9539de0f974bb80f998b9abf69
JonnaJK <96888686+JonnaJK@users.noreply.github.com>
2023-12-07 13:53:13 +0100

    Add implementation for ISavedGameService in SavedGameService class

---
+++ PathsOfPower/Services/SavedGameService.cs
@@ -0,0 +1,17 @@
+﻿using PathsOfPower.Interfaces;
+using PathsOfPower.Models;
+
+namespace PathsOfPower.Services;
+
+public class SavedGameService : ISavedGameService
+{
+    public SavedGame GetSavedGame(int slotNumber)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<SavedGame> GetSavedGames()
+    {
+        throw new NotImplementedException();
+    }
+}
