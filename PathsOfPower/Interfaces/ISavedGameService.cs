35afde4aa929e029226f210d6031139bbe71b792
JonnaJK <96888686+JonnaJK@users.noreply.github.com>
2023-12-07 13:51:14 +0100

    Add functions to ISavedGameService

--- PathsOfPower/Interfaces/ISavedGameService.cs
+++ PathsOfPower/Interfaces/ISavedGameService.cs
@@ -1,6 +1,9 @@
-﻿namespace PathsOfPower.Interfaces;
+﻿using PathsOfPower.Models;
+
+namespace PathsOfPower.Interfaces;
 
 public interface ISavedGameService
 {
-
+    List<SavedGame> GetSavedGames();
+    SavedGame GetSavedGame(int slotNumber);
 }
