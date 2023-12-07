ec2b07c7dde43022f1b4dd092cef4c3e18cf33b4
JonnaJK <96888686+JonnaJK@users.noreply.github.com>
2023-12-07 15:18:58 +0100

    Refactoring files
    Moved Program.cs from PathsOfPower.Core to PathsOfPower.Cli and moved interfaces from .Cli to .Core

--- PathsOfPower.Cli/Program.cs
+++ PathsOfPower.Cli/Program.cs
@@ -1,8 +1,20 @@
-﻿namespace PathsOfPower.Cli;
-internal class Program
-{
-    private static void Main(string[] args)
-    {
+﻿using PathsOfPower.Core;
+using PathsOfPower.Core.Helpers;
+using PathsOfPower.Core.Services;
 
+namespace PathsOfPower.Cli;
+
+public class Program
+{
+    public static void Main()
+    {
+        IConsoleWrapper consoleWrapper = new ConsoleWrapper();
+        IUserInteraction userInteraction = new UserInteraction(consoleWrapper);
+        var graphics = new Graphics();
+        IFileHelper fileHelper = new FileHelper();
+        IJsonHelper jsonHelper = new JsonHelper();
+        IQuestService questService = new QuestService(jsonHelper);
+        var game = new Game(userInteraction, graphics, fileHelper, questService, jsonHelper);
+        game.Run();
     }
 }
